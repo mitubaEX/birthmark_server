0ad20f7311abe3244a871078816f8555738b5f23
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2016-05-25 11:28:21 +0900

    third

--- barth.py
+++ barth.py
@@ -15,11 +15,13 @@ def do_upload():
     #file check
     #if ext in ('*.jar'):
         #print "this is jar"
-    upload.save("/Users/nakamurajun/barthmark_server/tmp",overwrite=True)
+    if os.path.isdir("./tmp") == False:
+        os.mkdir("./tmp")
+    upload.save("./tmp",overwrite=True)
     os.chdir("./barthmark")
     os.system("rm *.csv")
     for tmp in barthmark_kind:
-        os.system("java -jar /Users/nakamurajun/stigmata/target/stigmata-5.0-SNAPSHOT.jar -b "+tmp+" extract /Users/nakamurajun/barthmark_server/tmp/"+upload.filename+" > /Users/nakamurajun/barthmark_server/barthmark/"+upload.filename+"-"+tmp+".csv")
+        os.system("java -jar ../stigmata/target/stigmata-5.0-SNAPSHOT.jar -b "+tmp+" extract ../tmp/"+upload.filename+" > ./"+upload.filename+"-"+tmp+".csv")
     os.system("python birth_curl.py")
     os.system("python fuzzy_curl.py")
     return template("search")
