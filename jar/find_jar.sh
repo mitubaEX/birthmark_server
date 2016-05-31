1908b2d05320862a2608093be9d9d322c0bf3b06
ミツバEX <g1344955@cse.kyoto-su.ac.jp>
2016-05-31 21:52:30 +0900

    Update find_jar.sh

--- jar/find_jar.sh
+++ jar/find_jar.sh
@@ -10,7 +10,7 @@ do
         echo $abs
         filename=`basename ${file}`
         echo "$filename-$i.csv"
-        java -jar ~/birthmark_server/stigmata/target/stigmata-5.0-SNAPSHOT.jar -b $i extract "$abs/$filename" > "barthmark/$filename-$i.csv" ;
+        java -jar ~/birthmark_server/stigmata/target/stigmata-5.0-SNAPSHOT.jar -b $i extract "$abs/$filename" > "birthmark/$filename-$i.csv" ;
         java -jar ~/birthmark_server/stigmata/target/stigmata-5.0-SNAPSHOT.jar -b $i extract "$abs/$filename" > "fuzzy/$filename-$i.csv" ;
     done
     echo $i
