7c722b4a6b0535f3337dadc80b34e63c98dfff1b
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-05-03 13:13:47 +0900

    add calc threshold function in xml create script
    complete

--- script/xml_create/birthmark_xml_create_python3.py
+++ script/xml_create/birthmark_xml_create_python3.py
@@ -4,6 +4,7 @@ import sys
 
 # python3 ./birthmark_xml_create_python3.py <birthmark> <threshold>
 
+
 def get_hex_row(row_birthmark):
     if row_birthmark == "data" or row_birthmark == "":
         return row_birthmark
@@ -17,7 +18,22 @@ def get_hex_row(row_birthmark):
         hex_row += ','
     return hex_row
 
-threshold = int(sys.argv[2])
+
+def calcThreshold(birthmark):
+    sum_num = 0
+    count = 0
+    for i in glob.glob('../../data/birthmark/*' + birthmark + '*.csv'):
+        with open(i, 'r') as f:
+            import csv
+            for row in csv.reader(f):
+                if len(row) <= 1:
+                    continue
+                sum_num += len(row[3:])
+                count += 1
+    return int(sum_num / count / 3)
+
+
+threshold = int(calcThreshold(sys.argv[1]))
 output = open("../../data/birth_xml/birth_" + sys.argv[1] + ".xml", "w")
 output.write("<add>\n")
 output.write("<doc>\n")
@@ -35,7 +51,8 @@ for filename in glob.glob("../../data/birthmark/*" + sys.argv[1] + "*.csv"):
                     output.write("<doc>\n")
                     output.write("<field name=\"output\">"+row[0]+"</field>\n")
                     output.write("<field name=\"place\">"+row[1]+"</field>\n")
-                    output.write("<field name=\"barthmark\">"+row[2]+"</field>\n")
+                    output.write("<field name=\"barthmark\">" +
+                                 row[2]+"</field>\n")
                     output.write("<field name=\"encode_data\">"+encode_data.replace('<', '&lt;').replace(">", '&gt;').replace(
                         "&", '&amp;').replace("\"", '&quot;').replace("\'", '&apos;').replace("&quot;", "").replace("&amp;", "")+"</field>\n")
                     output.write("<field name=\"data\">"+','.join(row[3:]).replace('<', '&lt;').replace(">", '&gt;').replace(
@@ -46,7 +63,8 @@ for filename in glob.glob("../../data/birthmark/*" + sys.argv[1] + "*.csv"):
                     output.write("<doc>\n")
                     output.write("<field name=\"output\">"+row[0]+"</field>\n")
                     output.write("<field name=\"place\">"+row[1]+"</field>\n")
-                    output.write("<field name=\"barthmark\">"+row[2]+"</field>\n")
+                    output.write("<field name=\"barthmark\">" +
+                                 row[2]+"</field>\n")
                     output.write("<field name=\"data\">"+','.join(row[3:]).replace('<', '&lt;').replace(">", '&gt;').replace(
                         "&", '&amp;').replace("\"", '&quot;').replace("\'", '&apos;').replace("&quot;", "").replace("&amp;", "")+"</field>\n")
 
