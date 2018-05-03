f6c129fb4cf2d3489073a3ce9fe9cb716014ca1a
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-05-03 12:26:27 +0900

    add args of threshold

--- script/xml_create/birthmark_xml_create_python3.py
+++ script/xml_create/birthmark_xml_create_python3.py
@@ -2,6 +2,7 @@ import glob
 import csv
 import sys
 
+# python3 ./birthmark_xml_create_python3.py <birthmark> <threshold>
 
 def get_hex_row(row_birthmark):
     if row_birthmark == "data" or row_birthmark == "":
@@ -16,7 +17,7 @@ def get_hex_row(row_birthmark):
         hex_row += ','
     return hex_row
 
-
+threshold = int(sys.argv[2])
 output = open("../../data/birth_xml/birth_" + sys.argv[1] + ".xml", "w")
 output.write("<add>\n")
 output.write("<doc>\n")
@@ -24,16 +25,30 @@ output.write("<doc>\n")
 for filename in glob.glob("../../data/birthmark/*" + sys.argv[1] + "*.csv"):
     with open(filename, 'r') as f:
         for row in csv.reader(f):
-            encode_data = get_hex_row(','.join(row[3:]))
-            output.write("</doc>")
-            output.write("<doc>\n")
-            output.write("<field name=\"output\">"+row[0]+"</field>\n")
-            output.write("<field name=\"place\">"+row[1]+"</field>\n")
-            output.write("<field name=\"barthmark\">"+row[2]+"</field>\n")
-            output.write("<field name=\"encode_data\">"+encode_data.replace('<', '&lt;').replace(">", '&gt;').replace(
-                "&", '&amp;').replace("\"", '&quot;').replace("\'", '&apos;').replace("&quot;", "").replace("&amp;", "")+"</field>\n")
-            output.write("<field name=\"data\">"+','.join(row[3:]).replace('<', '&lt;').replace(">", '&gt;').replace(
-                "&", '&amp;').replace("\"", '&quot;').replace("\'", '&apos;').replace("&quot;", "").replace("&amp;", "")+"</field>\n")
+            if len(row[3:]) >= threshold:
+
+                # over threshold
+
+                if sys.argv[1] != "uc":
+                    encode_data = get_hex_row(','.join(row[3:]))
+                    output.write("</doc>")
+                    output.write("<doc>\n")
+                    output.write("<field name=\"output\">"+row[0]+"</field>\n")
+                    output.write("<field name=\"place\">"+row[1]+"</field>\n")
+                    output.write("<field name=\"barthmark\">"+row[2]+"</field>\n")
+                    output.write("<field name=\"encode_data\">"+encode_data.replace('<', '&lt;').replace(">", '&gt;').replace(
+                        "&", '&amp;').replace("\"", '&quot;').replace("\'", '&apos;').replace("&quot;", "").replace("&amp;", "")+"</field>\n")
+                    output.write("<field name=\"data\">"+','.join(row[3:]).replace('<', '&lt;').replace(">", '&gt;').replace(
+                        "&", '&amp;').replace("\"", '&quot;').replace("\'", '&apos;').replace("&quot;", "").replace("&amp;", "")+"</field>\n")
+                else:
+                    # encode_data = get_hex_row(','.join(row[3:]))
+                    output.write("</doc>")
+                    output.write("<doc>\n")
+                    output.write("<field name=\"output\">"+row[0]+"</field>\n")
+                    output.write("<field name=\"place\">"+row[1]+"</field>\n")
+                    output.write("<field name=\"barthmark\">"+row[2]+"</field>\n")
+                    output.write("<field name=\"data\">"+','.join(row[3:]).replace('<', '&lt;').replace(">", '&gt;').replace(
+                        "&", '&amp;').replace("\"", '&quot;').replace("\'", '&apos;').replace("&quot;", "").replace("&amp;", "")+"</field>\n")
 
 output.write("</doc>\n")
 output.write("</add>\n")
---
+++ script/xml_create/birthmark_xml_create_python3_script.sh
@@ -0,0 +1,6 @@
+python3 ./birthmark_xml_create_python3.py 2gram 15
+python3 ./birthmark_xml_create_python3.py 3gram 24
+python3 ./birthmark_xml_create_python3.py 4gram 31
+python3 ./birthmark_xml_create_python3.py 5gram 37
+python3 ./birthmark_xml_create_python3.py 6gram 41
+python3 ./birthmark_xml_create_python3.py uc 2
